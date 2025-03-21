e35359ab411baf20a4868361c45bddbf55c7ed6b
KaioHerculano <164403155+KaioHerculano@users.noreply.github.com>
2025-03-21 17:06:16 -0400

    I removed the comments from the code

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,9 @@ from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', CarsListView.as_view(), name='cars_list'),
+
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
